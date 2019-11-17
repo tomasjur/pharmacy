c9e214c0401d96dc7009049c6df790aaa3ad47df
tomasjur <tomasjkaunas@gmail.com>
2019-11-17 03:36:32 +0200

    Facebook OAuth2 (added missing files)

---
+++ app/Services/SocialFacebookAccountService.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomas
+ * Date: 2019-11-17
+ * Time: 02:33
+ */
+namespace App\Services;
+use App\SocialFacebookAccount;
+use App\User;
+use Laravel\Socialite\Contracts\User as ProviderUser;
+class SocialFacebookAccountService
+{
+    public function createOrGetUser(ProviderUser $providerUser)
+    {
+        $account = SocialFacebookAccount::whereProvider('facebook')
+            ->whereProviderUserId($providerUser->getId())
+            ->first();
+        if ($account) {
+            return $account->user;
+        } else {
+            $account = new SocialFacebookAccount([
+                'provider_user_id' => $providerUser->getId(),
+                'provider' => 'facebook'
+            ]);
+            $user = User::whereEmail($providerUser->getEmail())->first();
+            if (!$user) {
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $providerUser->getName(),
+                    'password' => md5(rand(1,10000)),
+                ]);
+            }
+            $account->user()->associate($user);
+            $account->save();
+            return $user;
+        }
+    }
+}
